7d925e2e0c21e2393f2f8644a9b10ff66d6e7fc6
fangcao1314 <1591041191@qq.com>
2016-01-22 11:38:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jackcao/androidtrain/MySurfaceView.java
@@ -0,0 +1,9 @@
+package com.example.jackcao.androidtrain;
+
+import android.view.SurfaceView;
+
+/**
+ * Created by jackcao on 2016-01-22.
+ */
+public class MySurfaceView extends SurfaceweVi{
+}
---
+++ app/src/main/java/com/example/jackcao/androidtrain/Myview.java
@@ -0,0 +1,65 @@
+package com.example.jackcao.androidtrain;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by jackcao on 2016-01-22.
+ */
+public class Myview extends View {
+     private  Paint paint;
+/*
+    public Myview(Context context) {
+        super(context);
+        inti();
+    }
+
+    public Myview(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        inti();
+    }
+    private  void inti(){
+       Paint    paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setAntiAlias(true);
+
+
+
+    }
+
+    @Override
+    public void draw(Canvas canvas){
+        super.draw(canvas);
+
+        drawText(canvas);
+
+    }
+
+    private void drawText(Canvas canvas) {
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(15);
+        canvas.drawCircle(200, 100, 100, paint);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(200,100,100,paint);
+
+    }
+      private void drawBitmap(Canvas canvas){
+        Bitmap  bitmap = BitmapFactory.decodeResource(getResources(),);
+    }
+    }
+
+
+
+
+
+
+*/
+}
